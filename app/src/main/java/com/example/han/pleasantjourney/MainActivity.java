6a58fa162a15ffa7a06dea92a375d46dc5ffd195
falcon-h <wongfalcon@gmail.com>
2015-05-04 01:08:13 +0800

    GUI updated. -Added toolbar. -Added cardView in activity_tracking.xml -Remove Overspeed from activity_tracking.xml -Reallign title & valuein activity_tracking.xml

--- app/src/main/java/com/example/han/pleasantjourney/MainActivity.java
+++ app/src/main/java/com/example/han/pleasantjourney/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,15 +18,15 @@ import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
         }
 
 /*       submit.setOnClickListener(new View.OnClickListener()
