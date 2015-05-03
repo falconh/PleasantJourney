c2c8f1efbb61916ed0f32ad7cae8982b6f91da04
falcon-h <wongfalcon@gmail.com>
2015-05-04 04:01:37 +0800

    GUI update -Combine lat and long to Coord -Combine Speed and overspeed to CurrentSpeed/SpeedLimit -Change Location Interval setting in Constants.java

--- app/src/main/java/com/example/han/pleasantjourney/Constants.java
+++ app/src/main/java/com/example/han/pleasantjourney/Constants.java
@@ -10,11 +10,11 @@ public class Constants {
     // Milliseconds per second
     private static final int MILLISECONDS_PER_SECOND = 1000;
     // Update frequency in seconds
-    private static final int UPDATE_INTERVAL_IN_SECONDS = 20;
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 5;
     // Update frequency in milliseconds
     public static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
     // The fastest update frequency, in seconds
-    private static final int FASTEST_INTERVAL_IN_SECONDS = 5;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 2;
     // A fast frequency ceiling in milliseconds
     public static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
     // Stores the lat / long pairs in a text file
